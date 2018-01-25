18852bf17e4b68183d0d2c664524fe52fdc15686
brilliant kaimba <briegrant416@gmail.com>
2018-01-25 23:15:26 +0300

    Add details to sources_test.py

--- app/tests/sources_test.py
+++ app/tests/sources_test.py
@@ -1,1 +1,21 @@
-sources_test.py
+import unittest
+# from app import app
+from app.models import Sources
+
+class SourcesTest(unittest.TestCase):
+    '''
+    Test class to test behaviours of the Sources class
+
+    Args:
+        unittest.TestCase : Test case class that helps create test cases
+    '''
+
+    def setUp(self):
+        """ Set up method to run before each test case """
+        self.news_source = Sources('abc-news-au','ABC News (AU)', 'Australia\'s most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.',"http://www.abc.net.au/news","general")
+
+    def test_instance(self):
+        '''
+        Test case to check if self.news_source is an instance of Source
+        '''
+        self.assertTrue( isinstance( self.news_source, Sources ) )
