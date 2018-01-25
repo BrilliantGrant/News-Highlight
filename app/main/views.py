f16c0b28c128236d6e1ced767aebc02d05d239f7
brilliant kaimba <briegrant416@gmail.com>
2018-01-25 23:27:51 +0300

    Add details to macros

--- app/main/views.py
+++ app/main/views.py
@@ -9,11 +9,15 @@ def index():
 	view root page function that returns the index page and its data
 	'''
 	 title = 'Home - Welcome to The best News Highlights Website Online'
-	return render_template('index.html',title = title)
+	news_sources = get_sources('sources')
+    return render_template('index.html', title=title, sources=news_sources)
 
 @main.route('/articles/<int:source_id>')
 def articles(source_id):
 	'''
 	view source page function that returns the news details page and its data
 	'''
-	return render_template('articles.html,'id = source_id)
+	title = f"{source_id} page"
+    #title = "Hello"
+    articles = get_articles(source_id)
+    return render_template('articles.html',title = title, articles = articles)
